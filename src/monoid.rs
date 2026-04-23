05fd0db076c03d9e35d200f1602fb8fb1c5b4d41
kitsuneninetails <kitsuneninetails@users.noreply.github.com>
2026-04-23 15:12:26 +0900

    Add rustdocs for Monoid and Semigroup

--- src/monoid.rs
+++ src/monoid.rs
@@ -5,6 +5,45 @@ use std::hash::Hash;
 use crate::{CFuture, semigroup::Semigroup};
 use futures::future::lazy;
 
+/// The Monoid Typeclass
+///
+/// Monoid describes the identity value for an implementing type.  Like Semigroup, a separate
+/// identity element for multiplication is available, in case the additive and multiplicative
+/// combination and identities are not identical.
+///
+/// To implement Monoid, a type must first implement the Semigroup typeclass as Monoid is an
+/// extension of Monoid.  Then, implement the `empty` and `empty_m` function.  The default
+/// implementation of empty_m just runs empty, in case there doesn't need to be a spearate
+/// implementaiton for multiplicative identity.
+///
+/// ```rust
+/// use rust_effects::{monoid::Monoid, semigroup::Semigroup};
+/// struct MyType(u32);
+///
+/// impl Semigroup for MyType {
+///   fn combine(a: Self, b: Self) -> Self {
+///     MyType(a.0 + b.0)
+///   }
+///
+///   fn combine_m(a: Self, b: Self) -> Self {
+///     MyType(a.0 * b.0)
+///   }
+/// }
+///
+/// impl Monoid for MyType {
+///     fn empty() -> Self {
+///         MyType(u32::empty())
+///     }
+///     fn empty_m() -> Self {
+///         MyType(u32::empty_m())
+///     }
+/// }
+///
+/// assert_eq!(MyType::empty().0, 0);
+/// assert_eq!(MyType::empty_m().0, 1);
+/// assert_eq!(MyType::combine(MyType(3), MyType::empty()).0, 3);
+/// assert_eq!(MyType::combine_m(MyType(3), MyType::empty_m()).0, 3);
+/// ```
 pub trait Monoid: Semigroup {
     fn empty() -> Self;
     fn empty_m() -> Self {
@@ -89,10 +128,45 @@ impl<'a, A: Monoid + Send + Sync + Clone + 'a> Monoid for CFuture<'a, A> {
     }
 }
 
+/// Global function for `empty`
+///
+/// Returns the empty implementation for the type T.  T can be inferred if `empty` is called
+/// and the result stored in a variable with type annotations or if empty is called as a
+/// function parameter (in which case, the type inference would know based on the parameter's
+/// type), otherwise, the type must be passed to `empty` when called.
+///
+/// Example:
+/// ```rust
+/// use rust_effects::monoid::empty;
+///
+/// assert_eq!(empty::<u32>(), 0);
+///
+/// let a: String = empty();
+/// assert_eq!(a, "");
+///
+/// fn add(a:u32, b: u32) -> u32 { a + b }
+/// assert_eq!(add(empty(), 3), 3);
+/// ```
 pub fn empty<T: Monoid>() -> T {
     T::empty()
 }
 
+/// Global function for `empty_m`.  
+///
+/// Returns the `empty_m` implementation for type T, returning the multiplicative identity
+/// for T.  As with `empty`, the type must be provided to the call unless the result is being
+/// used in such a way that the type can be inferred in reverse.
+/// /// ```rust
+/// use rust_effects::monoid::empty;
+///
+/// assert_eq!(empty_m::<u32>(), 01);
+///
+/// let a: String = empty_m();
+/// assert_eq!(a, "");
+///
+/// fn mul(a:u32, b: u32) -> u32 { a * b }
+/// assert_eq!(mul(empty_m(), 3), 3);
+/// ```
 pub fn empty_m<T: Monoid>() -> T {
     T::empty_m()
 }
--- src/semigroup.rs
+++ src/semigroup.rs
@@ -3,6 +3,37 @@ use std::{collections::HashMap, hash::Hash};
 use crate::CFuture;
 use futures::FutureExt;
 
+/// The Semigroup Typeclass
+///
+/// Semigroup describes the combination ability for two values of the implementing
+/// type (for both addition and multiplication).  
+///
+/// To implement Semigroup, a type must implement the `combine` and `combine_m` function.  
+/// The default implementation of combine_m just runs combine, in case there doesn't need to
+/// be a spearate implementaiton for multiplicative combine.
+///
+/// ```rust
+/// use rust_effects::semigroup::Semigroup;
+/// struct MyType(u32);
+///
+/// impl Semigroup for MyType {
+///   fn combine(a: Self, b: Self) -> Self {
+///     MyType(a.0 + b.0)
+///   }
+///
+///   fn combine_m(a: Self, b: Self) -> Self {
+///     MyType(a.0 * b.0)
+///   }
+/// }
+///
+/// let a = MyType(3);
+/// let b = MyType(4);
+/// assert_eq!(MyType::combine(a, b).0, 7);
+///
+/// let a = MyType(3);
+/// let b = MyType(4);
+/// assert_eq!(MyType::combine_m(a, b).0, 12);
+/// ```
 pub trait Semigroup: Sized {
     fn combine(a: Self, b: Self) -> Self;
     fn combine_m(a: Self, b: Self) -> Self {
@@ -122,10 +153,31 @@ impl<'a, A: 'a + Send + Sync + Clone + Semigroup> Semigroup for CFuture<'a, A> {
     }
 }
 
+/// Global `combine` function to combine two values of type T into a resulting T.
+///
+/// Type inference can almost always determine T from the parameters, so it's rarely
+/// necessary to specify manually.
+/// Example:
+///
+/// ```rust
+/// use rust_effects::semigroup::combine;
+/// assert_eq!(combine(3u32, 4u32), 7);
+/// assert_eq!(combine("Hello ".to_owned(), "world".to_owned()), "Hello world");
 pub fn combine<T: Semigroup>(a: T, b: T) -> T {
     T::combine(a, b)
 }
 
+/// Global `combine_m` function to multiplicatively combine two values of type T into
+/// a resulting T.
+///
+/// Type inference can almost always determine T from the parameters, so it's rarely
+/// necessary to specify manually.
+/// Example:
+///
+/// ```rust
+/// use rust_effects::semigroup::combine_m;
+/// assert_eq!(combine_m(3u32, 4u32), 12);
+/// assert_eq!(combine_m("Hello ".to_owned(), "world".to_owned()), "Hello world");
 pub fn combine_m<T: Semigroup>(a: T, b: T) -> T {
     T::combine_m(a, b)
 }
